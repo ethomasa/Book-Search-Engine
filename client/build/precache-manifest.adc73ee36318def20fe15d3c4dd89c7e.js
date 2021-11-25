e44afd488ce4591bedcd8bdbb0de1d137154d86e
Eby <ethomasa@gmail.com>
2021-11-24 21:21:35 -0600

    bug fixes

---
+++ client/build/precache-manifest.adc73ee36318def20fe15d3c4dd89c7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660066142a1a028382f2a7670d591205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6e001faff1490066172",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "70b0e3ebf3e852949bdd",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "b6e001faff1490066172",
+    "url": "/static/js/2.950493ea.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.950493ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70b0e3ebf3e852949bdd",
+    "url": "/static/js/main.c4f9ab5f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
